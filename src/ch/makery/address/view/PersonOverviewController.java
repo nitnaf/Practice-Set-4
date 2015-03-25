be64ef9b52756c889e95a07b4bcee1d30e145ec5
Steven Fantin <sfantin@udel.edu>
2015-03-25 16:54:04 -0400

    Commit

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -1,6 +1,8 @@
 package ch.makery.address.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
